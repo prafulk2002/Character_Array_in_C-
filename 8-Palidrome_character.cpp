66376cf7479eb8b74cfd593ed038780e57dd13dd
Praful Kumar <91059790+prafulk2002@users.noreply.github.com>
2022-04-06 18:10:07 +0530

    Add files via upload

---
+++ 8-Palidrome_character.cpp
@@ -0,0 +1,54 @@
+/*
+Check if a given character array is a palindrome or not.
+Palindrome: Given a string s, on reversing the string we get the same string we
+call that string is a palindrome.
+
+Algorithm:
+1) Let the length of the character array be n.
+2) Keep a boolean variable ans to store the result and initialize it with true.
+2) Iterate over the string and check if i
+
+th character is equal to (n-i-1)
+th
+, there can be
+
+2 cases
+a) If equal, then do nothing
+b) If unequal, then put ans = false
+3) When the loop ends, if ans is true, then the string is palindrome else it is not a
+palindrome.
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enterr the number of character:";
+    cin>>n;
+
+    char arr[n+1];
+    cout<<"Enter the Word :";
+    cin>>arr;
+
+    bool check=1;
+    for(int i=0 ; i<n ; i++)
+    {
+        if(arr[i]!=arr[n-1-i])
+        {
+            check=0;
+            break;
+        }
+    }
+
+    if(check)
+    {
+        cout<<"Word is palidrome";
+    }
+
+    else
+    cout<<"Word is not palidrome";
+
+    return 0;
+
+}
